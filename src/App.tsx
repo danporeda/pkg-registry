f507996287b578e98e4e59b9f6478d079589defa
Dan <danporeda@yahoo.com>
2024-06-10 14:39:12 -0400

    project structure refactor: created src/api/queries and /types. created queries/searchPackages to fetch npm resgistry packages into an interface, and mapped return as a PackageSummary type.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,16 @@ const router = createBrowserRouter([
       },
       {
         path: "/search",
-        element: <SearchPage />
+        element: <SearchPage />,
+        loader: async ({ request }) => {
+          const { searchParams } = new URL(request.url);
+          const term = searchParams.get('term');
+
+          if (!term) {
+            throw new Error('Search term must be provided');
+          }
+          
+        }
       },
       {
         path: "/packages/:name",
