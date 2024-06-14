cd19f691821ba3bae0047b8273993361f9a52f13
Dan <danporeda@yahoo.com>
2024-06-14 00:00:42 -0400

    imported homeLoader to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './pages/Root';
 import HomePage from './pages/home/HomePage';
+import { homeLoader } from './pages/home/homeLoader';
 import SearchPage from './pages/search/SearchPage';
 import { searchLoader } from './pages/search/searchLoader';
 import DetailsPage from './pages/details/DetailsPage';
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomePage />
+        element: <HomePage />,
+        loader: homeLoader
       },
       {
         path: "/search",
